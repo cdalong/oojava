914dafe18745f3760b122470779a62f19abf4ebf
Cam Long <kyameronlong@gmail.com>
2018-08-13 21:37:19 +0900

    initial commit

---
+++ Part1/Human.java
@@ -0,0 +1,35 @@
+public class Human {
+
+    String name;
+    int age;
+    int heightInInches;
+    String eyeColor;
+
+    //constructor
+    public Human(){
+
+        
+
+    }
+    public void speak(){
+
+        System.out.println("Hello my name is : " + name);
+
+        System.out.println(" I am " );
+
+
+
+
+    }
+    public void eat(){
+
+    }   
+    
+    public void walk(){
+
+
+    }
+    public void work(){
+
+    }
+}
